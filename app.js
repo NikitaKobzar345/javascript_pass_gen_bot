6098fd63216635266d34f47a28a0bdc8bc35baf3
Никита <76733643+NikitaKobzar345@users.noreply.github.com>
2021-05-29 14:02:35 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 require('dotenv').config();
-const { Telegraf, Markup, Context } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
+const utf8=require('utf8')
+const config=require('./config')
 const app = new Telegraf(process.env.BOT_TOKEN);
-const { passwordStrength } = require('check-password-strength');
+
+function generatePasswordHard(lenght) {
+  let sep = config.sumbols.split("");
+  let k, result;
+  for (let i = sep.length - 1; i > 0; i--) {
+    k = Math.floor(Math.random() * (i + 1));
+    result = sep[k];
+    sep[k] = sep[i];
+    sep[i] = result;
+  }
+
+  return sep.slice(0, lenght).join("");
+}
 
 
 app.start((ctx) =>
@@ -15,6 +29,8 @@ app.start((ctx) =>
 Например: 
 вход-cucushka,curit,voditel
 выход-cUCu3hk@cuR1tVod1t3l
+
+Учти что я не принимаю эмодзи!
 `,
     Markup.keyboard([
       ["Сгенерировать сложный пароль"],["Сгенерировать простой пароль"],
@@ -25,76 +41,49 @@ app.start((ctx) =>
 );
 
 
-app.hears("Сгенерировать сложный пароль",(ctx)=>{
- 
- let password="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!№;%:?*()_+="
+app.hears("Сгенерировать сложный пароль",(ctx)=>
+ctx.reply(`Ваш пароль:  ${generatePasswordHard(20)}`))
 
-  function generateHardPassword(password){
+app.hears("Сгенерировать простой пароль", (ctx) =>
+  ctx.reply(`Ваш пароль:  ${generatePasswordHard(8)}`)
+);
 
-    let arr=password.split('');
-      let j, temp;
-      for(let i = arr.length-1; i > 0; i--){
-        j = Math.floor(Math.random()*(i + 1));
-        temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-      }
-
-      return arr.slice(0,20).join('');
-
-  }
-  {ctx.reply(`Ваш пароль :   ${generateHardPassword(password)}`)};
-    
-})
-
-
-app.hears("Сгенерировать простой пароль",(ctx)=>{
- 
-  let password="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!№;%:?*()_+="
- 
-   function generateSimplePassword(password){
- 
-     let sep=password.split('');
-       let k, result;
-       for(let i = sep.length-1; i > 0; i--){
-         k = Math.floor(Math.random()*(i + 1));
-         result = sep[k];
-         sep[k] = sep[i];
-         sep[i] = result;
-       }
- 
-       return sep.slice(0,8).join('');
- 
-   }
-   {ctx.reply(`Ваш пароль :    ${generateSimplePassword(password)}`)};
-   
- })
- 
-app.hears("Сгенерировать из ключевых слов",(ctx)=>{
-    {ctx.reply('Окей,вводи слова')}
-    
- })
+app.hears("Сгенерировать из ключевых слов", (ctx) =>
+  ctx.reply("Окей,вводи слова")
+);
 
  app.on('text', (ctx) => {
 
 let text=ctx.message.text
 
-      function genAPassFromKeyWords(text){
-      let replacedSymbols= text.replace('a','@').replace('o','0').replace('z','2').replace('e','3')
-        .replace('s','2').replace('i','1').replace('b','8')
 
-   let upcase=replacedSymbols.split('').reduce((a, c) => a + (/[ugdkrctyfwvzlphsim]/i.test(c) ? c.toUpperCase() : c.toLowerCase()), "")
-      
-   let array=upcase.split('')
-
-      array.splice(Math.floor(Math.random()*array.length), 0,"*")
-      array.splice(Math.floor(Math.random()*array.length), 0,"%")
-      array.splice(Math.floor(Math.random()*array.length),0,'#')
-      array.splice(Math.floor(Math.random()*array.length),0,'$')
-    
-      return array.join('')
-     
+    function genAPassFromKeyWords(text) {
+      for (const [key, value] of Object.entries(config.toReplace)) {
+        text = text.replace(key, value);
+      }
+  
+      text = text
+        .split("")
+        .reduce(
+          (a, c) =>
+            a +
+           ((Math.floor(Math.random() * text.length)) ? c.toUpperCase() : c.toLowerCase()),
+          ""
+        );
+  
+      text = text.split("");
+  
+      for (i = 0; i < Math.floor(text.length / 3); i++) {
+        let additional = "*$%#";
+        text.splice(
+          Math.floor(Math.random() * text.length),
+          0,
+          additional[Math.floor(Math.random() * additional.length)]
+        );
+      }
+      return text.join("");
     }
-    {ctx.reply(`Ваш пароль :    ${genAPassFromKeyWords(text)}`)}
-})
+      ctx.reply(`Ваш пароль:  ${genAPassFromKeyWords(text)}`);
+    
+  })
 app.launch();

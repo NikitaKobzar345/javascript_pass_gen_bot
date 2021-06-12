9d580d207924dd7996972e000504b10e6e263494
Lazarus <76733643+NikitaKobzar345@users.noreply.github.com>
2021-06-12 20:16:18 +1000

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ const app = new Telegraf(process.env.BOT_TOKEN);
 
 const emoji = emojiRegex()
 
-
-function generatePassword(lenght) {
+function generatePasswordHard(lenght) {
   let sep = config.sumbols.split("");
   let k, result;
+  
   for (let i = sep.length - 1; i > 0; i--) {
+
     k = Math.floor(Math.random() * (i + 1));
     result = sep[k];
-    sep[k] = sep[i]
+    sep[k] = sep[i];
     sep[i] = result;
-  }
 
+  }
   return sep.slice(0, lenght).join("");
 }
 
@@ -45,10 +46,10 @@ app.start((ctx) =>
 
 
 app.hears("Сгенерировать сложный пароль",(ctx)=>
-ctx.reply(`Ваш пароль:  ${generatePassword(20)}`))
+  ctx.reply(`Ваш пароль:  ${generatePasswordHard(20)}`))
 
 app.hears("Сгенерировать простой пароль", (ctx) =>
-  ctx.reply(`Ваш пароль:  ${generatePassword(8)}`)
+  ctx.reply(`Ваш пароль:  ${generatePasswordHard(8)}`)
 );
 
 app.hears("Сгенерировать из ключевых слов", (ctx) =>
@@ -59,47 +60,46 @@ app.hears("Сгенерировать из ключевых слов", (ctx) =>
 
 let text=ctx.message.text
 
-
-    if(text.match(emoji)){
-    ctx.reply('Я не принимаю эмодзи')
-    return
+  if(text.match(emoji)){
+  ctx.reply('Я не принимаю эмодзи')
+  return
 }
-    if(text.length >= 30){
-      ctx.reply('Слишком много букав')
-} else {
 
-      function genAPassFromKeyWords(text) {
-        for (const [key, value] of Object.entries(config.toReplace)) {
-          text = text.replace(key, value);
-        }
+  function genAPassFromKeyWords(text) {
+    for (const [key, value] of Object.entries(config.toReplace)) {
+      text = text.replace(key, value);
+  }
+
+    text = text
+      .split("")
+      .reduce(
+        (a, c) =>
+          a +
+        ((Math.floor(Math.random() * text.length)) ? c.toUpperCase() : c.toLowerCase()),
+        ""
+      );
+
+    text = text.split("");
+
+    let additional='$@#?*';
+
+    for (i = 0; i < Math.floor(text.length / 3 ); i++) {
       
-        text = text
-          .split("")
-          .reduce(
-            (a, c) =>
-              a +
-             ((Math.floor(Math.random() * text.length)) ? c.toUpperCase() : c.toLowerCase()),
-            ""
-          );
-    
-        text = text.split("");
-  
-        let additional='$@#?*';
-  
-        for (i = 0; i < Math.floor(text.length / 3 ); i++) {
-          
-          text.splice(
-            Math.floor(Math.random() * text.length),
-            0,
-            additional[Math.floor(Math.random() * additional.length)]
-          );
-        }
-  
-        return text.join('');
-  
-      }
-        ctx.reply(`Ваш пароль:  ${genAPassFromKeyWords(text)}`);
+      text.splice(
+        Math.floor(Math.random() * text.length),
+        0,
+        additional[Math.floor(Math.random() * additional.length)]
+      );
     }
-  })
+    return text.join('');
+}
+
+  if(text.length >= 30){
+    ctx.reply('Слишком много символов')
+
+} else {
+    ctx.reply(`Ваш пароль:  ${genAPassFromKeyWords(text)}`)
+   }
+})
 
 app.launch();

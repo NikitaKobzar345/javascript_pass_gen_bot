1d4b682d9f1858a6380d468a0b84174417b4fff7
Lazarus <76733643+NikitaKobzar345@users.noreply.github.com>
2021-06-26 18:40:21 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,5 @@
 let sumbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!№;%:?*()_+=";
-let toReplace = {
-    a: "@",
-    o: "0",
-    z: "2",
-    e: "3",
-    s: "2",
-    i: "1",
-    b: "8",
-    а: "@",
-    в: "8",
-    м: "w",
-    е:'3',
-    о:'0',
-    в:'8',
-    у:'u',
-    я:'r'
-
-  };
 
 module.exports = {
     sumbols,
-    toReplace,
 }
